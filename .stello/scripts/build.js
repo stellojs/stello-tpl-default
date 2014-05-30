cc6dc7226c8d07182e114f8fe571c0abc517053b
Justin <jus.russell@gmail.com>
2014-05-30 17:48:43 -0400

    feature: Migrating to scripted metalsmith flow

--- .stello/scripts/build.js
+++ .stello/scripts/build.js
@@ -8,19 +8,34 @@ var markdown = require('metalsmith-markdown')
   , templates = require('metalsmith-permalinks')
   , collections = require('metalsmith-collections');
 
-metalsmith
-  .metadata({
-    // Get metadata from .stellorc?
-    title: 'My Stello Site',
-    description: 'Made with <3'
-  })
-  .use(markdown())
-  .use(collections({
-    posts: {},
-    pages: {}
-  }))
-  .use(permalinks({
-    pattern: ':title'
-  }))
-  .use(templates('handlebars'))
-  .build();
+var async = require('async');
+
+var registerHelpers = function() {
+  // - find ../../templates/_*.hbs
+  // - convert snake-case to camelCase
+  // - register as helper
+
+  //async.waterfall
+
+};
+
+var doBuild = function() {
+  metalsmith
+    .metadata({
+      // Get metadata from .stellorc?
+      title: 'My Stello Site',
+      description: 'Made with <3'
+    })
+    .use(markdown())
+    .use(collections({
+      posts: {},
+      pages: {}
+    }))
+    .use(permalinks({
+      pattern: ':title'
+    }))
+    .use(templates('handlebars'))
+    .build();
+};
+
+doBuild();
