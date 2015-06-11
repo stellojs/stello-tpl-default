2457070fb18160eb96e7619db06034729dcd21f2
jtrussell <jus.russell@gmail.com>
2015-06-10 21:27:38 -0400

    refactor: Clean up to reflect a simpler stello api

---
+++ index.js
@@ -0,0 +1,12 @@
+
+/**
+ * The build routine
+ *
+ * Invoked by the stello harness when `stello build` is run
+ *
+ * @param {StelloCore} stello A stello instance
+ * @param {Function} cb The callback
+ */
+exports.build = function build(stello, cb) {
+  // build the site... :)
+};
