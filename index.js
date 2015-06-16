6c8bf47bc98a6e5c92eb01ee43dc8aaa178ce1e3
Justin Russell <jus.russell@gmail.com>
2015-06-15 21:14:45 -0400

    refactor: Promisify

--- index.js
+++ index.js
@@ -8,5 +8,26 @@
  * @param {Function} cb The callback
  */
 exports.build = function build(stello, cb) {
-  // build the site... :)
+  var P = require('bluebird')
+    , fs = P.promisifyAll(require('fs'))
+    , Handlebars = require('handlebars')
+    , path = require('path')
+    , utf8 = {encoding: 'utf8'};
+
+  var tpl = function(p) {
+    return path.join(__dirname, 'templates', p);
+  };
+
+  P.props({
+    layout: fs.readFileAsync(tpl('layouts/default.hbs'), utf8),
+    page: fs.readFileAsync(tpl('partials/page.hbs'), utf8),
+    post: fs.readFileAsync(tpl('partials/post.hbs'), utf8)
+  })
+  .then(function(results) {
+    var layout = results.layout;
+    Handlebars.registerPartial('page', results.page);
+    Handlebars.registerPartial('post', results.post);
+
+    console.log('Build!');
+  });
 };
